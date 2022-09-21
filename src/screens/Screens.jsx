eabf99991aaacf4413b99e8b7e7a87be78d34f6a
Aydaruus Mouse <halalmedia9@gmail.com>
2022-09-21 10:57:57 +0300

    update

--- src/screens/Screens.jsx
+++ src/screens/Screens.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
-import Products from '../products'
+import axios from 'axios' 
 import Product from '../components/Products'
+
 function Screens() {
+  const [Products, setProducts] = useState([])
+
+  useEffect(()=>{
+    const fetchProducts= async()=>{
+      const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
   return (
     <div>
         <h1>Latest Products</h1>
--- src/screens/productScreen.jsx
+++ src/screens/productScreen.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import products from '../products'
+// import products from '../products'
 import {useParams} from 'react-router-dom'
 import {Card, Image, ListGroup, Button, Row, Col, ListGroupItem} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import Rating from '../components/Rating'
+import {useState, useEffect} from 'react'
+import axios from 'axios' 
 function productScreen() {
- const pram= useParams()
- const product= products.find(product=> product._id== pram.id)
+  const [product, setProduct] = useState([])
+  const pram= useParams()
+  useEffect(()=>{
+    const fetchProduct= async()=>{
+      const {data} = await axios.get(`http://127.0.0.1:8000/api/products/${pram.id}/`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
+
+//  const product= products.find(product=> product._id== pram.id)
   return (
     <div>
         <Link to="/" className="btn btn-light my-3">Go Back </Link>
@@ -14,13 +25,13 @@ function productScreen() {
             <Col md={6}>
                 <Image src={product.image} alt={product.name} fluid/>
             </Col>
-            <Col md={5}>
+            <Col md={3}>
                 <ListGroup varinat="flush">
                   <ListGroupItem>
                     <h3>{product.name}</h3>
                   </ListGroupItem>
                   <ListGroupItem>
-                    <Rating value={product.rating} text={product.numReviews} color={"#f8e825"}></Rating>
+                    <Rating value={product.rating} text={`${product.numReviews} Rewiews`} color={"#f8e825"}></Rating>
                   </ListGroupItem>
                   <ListGroupItem>
                     Price: ${product.price}
@@ -28,10 +39,40 @@ function productScreen() {
                   <ListGroupItem>
                     Description: {product.description}
                   </ListGroupItem>
-                  <Button>Order</Button>
                 </ListGroup>
             </Col>
+          <Col md={3}>
+             <Card>
+                <ListGroup>
+                  <ListGroupItem>
+                    <Row>
+                      <Col>
+                        Price:
+                      </Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <Row>
+                      <Col>
+                        Status:
+                      </Col>
+                      <Col>
+                          {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                      </Col>
+                    </Row>
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <Row>
+                    <Button className="btn-block center" type="button" disabled={product.countInStock == 0}>Add To Cart</Button>
 
+                    </Row>
+                  </ListGroupItem>
+                </ListGroup>
+             </Card>
+          </Col>
         </Row>
     </div>
   )
